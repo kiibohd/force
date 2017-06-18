83b753d7eb02753fa95596e7831cb49901a88cb3
Jacob Alexander <triplehaata@gmail.com>
2017-06-18 14:27:35 -0700

    Initial Plotly->GDoc sync script
    - Queries the given Plotly user's switches
    - Queries switches with/without meta on Plotly
    - Queries the given Google Doc spreadsheet for switches

---
+++ Plotly/README.md
@@ -0,0 +1,56 @@
+# Plotly API Sync
+
+Another way to interact with the plotly data is using their API.
+This has various conveniences, such as being able to query all measured switches directly.
+
+**Note**: You'll quickly run out of your daily plotly API usage with a free account using these scripts!
+
+
+## Requirements
+
+* Python 3
+* oauth2client (Python)
+* gspread (Python)
+* requests (Python)
+
+
+## Configuration
+
+You'll need to setup a configuration file with the necessary API keys.
+By default the configuration file is stored at `~/.config/plotly_gdoc_sync.json`.
+
+For example:
+
+```json
+{
+        "plotly_user" : "haata",
+        "plotly_api_key" : "<plotly api key>",
+        "gdoc_oauth_json" : "<local google doc oauth json>",
+        "gdoc_url" : "https://docs.google.com/spreadsheets/d/<myhash>/edit?usp=sharing",
+        "gdoc_scope" : ["https://spreadsheets.google.com/feeds"]
+}
+```
+
+
+### Ploty
+
+To generate a Plotly API key, create your own account (Plotly)[https://plot.ly/feed].
+Then on the top-right of the Plotly page (your username), click on **Settings**.
+On the left click on **API Keys** and click on **Regenerate Keys**.
+
+Use that key as the `plotly_api_key`. The `plotly_user` should be the user you're trying to query, which may/or may not be your user.
+
+
+### Google Doc
+
+See the gspread (documentation)[http://gspread.readthedocs.io/en/latest/oauth2.html] on generating the `gdoc_oauth_json` file.
+The `gdoc_url` is just the URL to the google spreadsheet you're trying to synchronize with.
+
+
+## Usage
+
+```bash
+./plotly_gdoc_sync.py --help
+
+```
+
---
+++ Plotly/plotly_gdoc_sync.py
@@ -0,0 +1,303 @@
+#!/usr/bin/env python3
+# Jacob Alexander 2017
+# Uses Plotly and Google Sheets APIs to synchronize a google doc spreadsheet
+# Data may entered into the google doc spreadsheet separately
+
+import argparse
+import concurrent.futures
+import json
+import os
+import sys
+import urllib.parse as urlparse
+
+import gspread
+import requests
+
+from oauth2client.service_account import ServiceAccountCredentials
+from requests.auth import HTTPBasicAuth
+
+
+
+# Print Decorator Variables
+ERROR = '\033[5;1;31mERROR\033[0m:'
+WARNING = '\033[1;33mWARNING\033[0m:'
+
+
+## Python Text Formatting Fixer...
+##  Because the creators of Python are averse to proper capitalization.
+textFormatter_lookup = {
+	"usage: "              : "\033[1mUsage\033[0m: ",
+	"optional arguments"   : "\033[1mOptional Arguments\033[0m",
+	"positional arguments" : "\033[1mPositional Arguments\033[0m",
+}
+
+def textFormatter_gettext( s ):
+	return textFormatter_lookup.get( s, s )
+
+argparse._ = textFormatter_gettext
+
+
+# Parse Arguments
+parser = argparse.ArgumentParser(
+	formatter_class=argparse.RawDescriptionHelpFormatter,
+)
+parser.add_argument("--config",
+	default=os.path.expanduser("~/.config/plotly_gdoc_sync.json"),
+	help="Config file containing API keys and google doc",
+)
+parser.add_argument("--list-plotly-switches",
+	action="store_true",
+	help="List switches stored in plotly",
+)
+parser.add_argument("--list-plotly-missing-metadata",
+	action="store_true",
+	help="List switches in plotly missing metadata",
+)
+parser.add_argument("--list-plotly-metadata",
+	action="store_true",
+	help="List switches in plotly with metadata",
+)
+parser.add_argument("--list-gdoc-switches",
+	action="store_true",
+	help="List switches stored in gdoc",
+)
+parser.add_argument("--sync-plotly-to-gdoc",
+	action="store_true",
+	help="Synchronizes Plotly data to Google Doc.",
+)
+
+
+args = parser.parse_args()
+options = {}
+if os.path.exists( args.config ):
+	with open( args.config ) as fp:
+		options = json.load( fp )
+else:
+	print( "{0} '{1}' doesn't exist. Must specify configuration file for API keys.".format( ERROR, args.config ) )
+
+
+
+class PlotlyData:
+	'''
+	Class for reading switch data from Plotly
+	'''
+	def __init__( self ):
+		# Authenticate with Plotly
+		self.auth = HTTPBasicAuth( options['plotly_user'], options['plotly_api_key'] )
+		self.headers = { 'Plotly-Client-Platform' : 'python' }
+
+	def read_url( self, url ):
+		r = requests.get(
+			url,
+			auth=self.auth,
+			headers=self.headers
+		)
+		if r.status_code != 200:
+			print( "{0} Could not retrieve Plotly folders: {1}".format( ERROR, r ) )
+			print( url )
+			sys.exit( 1 )
+		return r
+
+	def get_all_pages( self, url ):
+		data = []
+
+		# Get initial page
+		r = self.read_url( url.format( 1 ) )
+		data.append( json.loads( r.text )['children'] )
+
+		# Determine total pages to read
+		try:
+			next_page = int( urlparse.parse_qs( urlparse.urlparse( data[-1]['next'] ).query )['page'][0] )
+			last_page = int( urlparse.parse_qs( urlparse.urlparse( data[-1]['last'] ).query )['page'][0] )
+		except:
+			# There may not be any next/last pages
+			return data
+
+		urls = [ url.format( num ) for num in range( next_page, last_page + 1 ) ]
+
+		# Concurrently read generated list of urls
+		with concurrent.futures.ThreadPoolExecutor( max_workers=10 ) as executor:
+			future_to_url = {
+				executor.submit( self.read_url, url ) : url for url in urls
+			}
+			for future in concurrent.futures.as_completed( future_to_url ):
+				url = future_to_url[ future ]
+				try:
+					data.append( json.loads( future.result().text )['children'] )
+				except Exception as exc:
+					print( "{0} generated an exception: {1}".format( ERROR, exc ) )
+		return data
+
+	def get_all_folders( self ):
+		# Get all page data
+		data = self.get_all_pages('https://api.plot.ly/v2/folders/home?page={}&world_readable=true')
+
+		# Parse over results and filter out folders
+		folders = {}
+		for elem in data:
+			for result in elem['results']:
+				if result['filetype'] == 'fold':
+					folders[ result['api_urls']['folders'] ] = result['filename']
+
+		return folders
+
+	def get_plots_in_folders( self, url, folder ):
+		# Get all page data
+		data = self.get_all_pages( "{0}?page={{}}".format( url ) )
+
+		# Gather results, only grid and plots
+		results = []
+		for elem in data:
+			for result in elem['results']:
+				if result['filetype'] == 'plot' or result['filetype'] == 'grid':
+					results.append( result )
+
+		# Return a tuple so we know where the data came from
+		return (results, url)
+
+	def get_switches( self, verbose=False ):
+		#print( json.dumps( data[-1], sort_keys=True, indent=4 ) )
+		#print( json.dumps( data['children'], sort_keys=True, indent=4 ) )
+		# Get list of folders (world readable)
+		folders = self.get_all_folders()
+
+		# Get list of switches
+		switches = {}
+
+		# Parallelize API calls to retrieve list of switches
+		with concurrent.futures.ThreadPoolExecutor( max_workers=100 ) as executor:
+			future_to_url = { executor.submit( self.get_plots_in_folders, *item ) : item for item in folders.items() }
+			for future in concurrent.futures.as_completed( future_to_url ):
+				url = future_to_url[ future ]
+				try:
+					result = future.result()
+					# Returns a tuple, use the url as the dictionary key
+					switches[ result[1] ] = result[0]
+				except Exception as exc:
+					print( "{0} generated an exception: {1}".format( ERROR, exc ) )
+
+		# Sort by folder name
+		total_switches = 0
+		for url, name in sorted( folders.items(), key=lambda items: items[1] ):
+			if verbose:
+				print( name, url )
+			switch_list = []
+			for elem in switches[ url ]:
+				if elem['filetype'] == 'plot':
+					switch_list.append( elem )
+					total_switches += 1
+			for switch in sorted( switch_list, key=lambda items: items['filename'] ):
+				if verbose:
+					print( "\t{0}".format( switch['filename'] ) )
+
+		# Print total number of switch plots
+		if verbose:
+			print( "Total Switches:", total_switches )
+
+		return switches, folders
+
+	def check_switch_metadata( self, switch, has ):
+		new_url = switch['api_urls']['grids']
+		json_data = json.loads( self.read_url( new_url ).text )
+		#print( json.dumps( json_data, sort_keys=True, indent=4 ) )
+		metadata_len = len( json_data['metadata'].keys() )
+		if has and metadata_len > 0 or not has and metadata_len == 0:
+			print( "{0} - {1}".format( json_data['filename'], json_data['fid'] ) )
+
+
+	def check_all_switch_metadata( self, has=False ):
+		switches, folders = p_data.get_switches()
+		switch_list = []
+
+		# Sort by Vendor
+		for url, name in sorted( folders.items(), key=lambda items: items[1] ):
+			# Find grid files to build lists
+			for elem in switches[ url ]:
+				if elem['filetype'] == 'grid':
+					switch_list.append( elem )
+
+		# Parallelize API calls to retrieve list of switches
+		with concurrent.futures.ThreadPoolExecutor( max_workers=100 ) as executor:
+			future_to_url = { executor.submit( self.check_switch_metadata, switch, has ) : switch for switch in switch_list }
+			for future in concurrent.futures.as_completed( future_to_url ):
+				url = future_to_url[ future ]
+				try:
+					result = future.result()
+					# Returns a tuple, use the url as the dictionary key
+					# TODO
+				except Exception as exc:
+					print( "{0} generated an exception: {1}".format( ERROR, exc ) )
+
+
+class GDocData:
+	'''
+	Class for read/writing to google doc spreadsheets
+	'''
+	def __init__( self ):
+		self.credentials = ServiceAccountCredentials.from_json_keyfile_name(
+			options['gdoc_oauth_json'],
+			options['gdoc_scope'],
+		)
+		self.gc = gspread.authorize( self.credentials )
+		self.sheet = self.gc.open_by_url( options['gdoc_url'] )
+		self.worksheet = self.sheet.get_worksheet(0)
+
+	def get_switches( self, verbose=False ):
+		# Get list of switches
+		switches = {}
+
+		# Iterate over each switch entry
+		for switch in self.worksheet.get_all_records():
+			name = switch['Popular Name']
+			if switch['Part Number'] != "":
+				name = "{0} {1}".format( name, switch['Part Number'] )
+
+			# Insert switch into dictionary
+			if switch['Brand'] not in switches.keys():
+				switches[ switch['Brand'] ] = [ ( name, switch ) ]
+			else:
+				switches[ switch['Brand'] ].append( ( name, switch ) )
+
+		# Sort by brand
+		for brand, elem in sorted( switches.items(), key=lambda items: items[0] ):
+			if verbose:
+				print( "{0} Switches".format( brand ) )
+			for switch in sorted( elem, key=lambda items: items[0] ):
+				if verbose:
+					print( "\t{0} {1}".format( brand, switch[0] ) )
+
+		return switches
+
+
+### Main ###
+
+# Initialize APIs
+p_data = PlotlyData()
+g_data = GDocData()
+
+
+# List plotly switches if requested
+if args.list_plotly_switches:
+	p_data.get_switches( verbose=True )
+	sys.exit(0)
+
+# List gdoc switches if requested
+if args.list_gdoc_switches:
+	g_data.get_switches( verbose=True )
+	sys.exit(0)
+
+# List plotly switches missing metadata
+if args.list_plotly_missing_metadata:
+	p_data.check_all_switch_metadata( False )
+	sys.exit(0)
+
+# List plotly switches with metadata
+if args.list_plotly_metadata:
+	p_data.check_all_switch_metadata( True )
+	sys.exit(0)
+
+# Synchronize Plotly to Google Doc
+if args.sync_plotly_to_gdoc:
+	# TODO
+	pass
+
